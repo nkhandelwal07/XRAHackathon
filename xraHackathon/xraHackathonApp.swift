15ccefd970e4bdac9e89a5c14cbe7c97e0c2bd0a
nidhi.partha <nidhi.parthasarathy@gmail.com>
2026-04-18 13:23:50 -0700

    commit

--- xraHackathon/xraHackathonApp.swift
+++ xraHackathon/xraHackathonApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct xraHackathonApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FreeFormDrawingView()
         }
         .windowStyle(.volumetric)
     }
